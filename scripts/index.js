d5cae6350f13a5f8dce093ddabf57fad7f04af90
serp123616512 <serp123616512@yandex.ru>
2022-11-29 14:25:27 +0300

    fix: new js file cards

--- scripts/index.js
+++ scripts/index.js
@@ -18,32 +18,6 @@ const popupPictureImage = popupElementPicture.querySelector('.popup__pic');
 const popupPictureTitle = popupElementPicture.querySelector('.popup__title');
 const cardsListElement = document.querySelector('.cards');
 const cardTemplateElement = document.querySelector('#card-template').content;
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
 const openPopup = popup => {
   popup.classList.add('popup_status_open');
